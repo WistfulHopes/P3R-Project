9631d93e9ac0295adea8ba4751b9fd76826bf17f
TheBestAstroNOT <139786546+TheBestAstroNOT@users.noreply.github.com>
2024-12-04 20:41:51 +0530

    Update StaffRollTableData.cpp
    Most of the rows i see in the data assets have this value
    so if anyone ever edits them they will have a easier time and would not have to type these almost universal values repeatedly

--- Source/xrd777/Private/StaffRollTableData.cpp
+++ Source/xrd777/Private/StaffRollTableData.cpp
@@ -2,14 +2,18 @@
 
 FStaffRollTableData::FStaffRollTableData() {
     this->StaffRollIndex = 0;
+    this->Ficolor = FColor(0, 0, 0, 255);
+    this->Scolor = FColor(0, 0, 0, 255);
+    this->Tcolor = FColor(0, 0, 0, 255);
+    this->Focolor = FColor(0, 0, 0, 255);
     this->Fistyle = 0;
     this->Sstyle = 0;
     this->Tstyle = 0;
     this->Fostyle = 0;
-    this->Fisize = 0;
-    this->Ssize = 0;
-    this->Tsize = 0;
-    this->Fosize = 0;
+    this->Fisize = 1;
+    this->Ssize = 1;
+    this->Tsize = 1;
+    this->Fosize = 1;
     this->Command = 0;
     this->SecondCommand = 0;
     this->ThirdCommand = 0;
@@ -17,7 +21,7 @@ FStaffRollTableData::FStaffRollTableData() {
     this->LineCount = 0;
     this->LineCommand = false;
     this->EmptyCount = 0;
-    this->StartWaitSeconds = 0.00f;
+    this->StartWaitSeconds = 8.00f;
     this->FinishSeconds = 0.00f;
     this->LastSeconds = 0.00f;
 }
